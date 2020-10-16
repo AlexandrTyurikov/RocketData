41a48f906a4530b6960178f12e757907451cb659
AlexandrTyurikov <48610264+AlexandrTyurikov@users.noreply.github.com>
2020-10-16 08:36:46 +0300

    test_task
    test_task

---
+++ parser_site_1.py
@@ -0,0 +1,30 @@
+import requests
+from bs4 import BeautifulSoup as bs
+import json
+
+headers = {
+    "user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36",
+    'accept': '*/*'
+}
+url_mebelshara = "https://www.mebelshara.ru/contacts"
+date = []
+
+
+def mebelshara_pars():
+    request = requests.get(url_mebelshara, headers=headers)
+    if request.status_code == 200:
+        soup = bs(request.content, 'lxml')
+        salons_info = soup.find_all('div', attrs={'class': 'shop-list-item'})
+        for salon in salons_info:
+            date.append({
+                'address': salon['data-shop-address'],
+                'latlon': [float(salon['data-shop-latitude']), float(salon['data-shop-longitude'])],
+                'name': salon['data-shop-name'],
+                'phones': salon['data-shop-phone'],
+                'working_hours': f"{salon['data-shop-mode1']} {salon['data-shop-mode2']}"
+            })
+    with open('data_site_1.json', 'w') as file:
+        json.dump(date, file, indent=4, ensure_ascii=False)
+
+
+mebelshara_pars()
---
+++ parser_site_2.py
@@ -0,0 +1,88 @@
+import requests
+import json
+import sys
+import time
+import threading
+
+"""class Spinner - визуализация ожидания"""
+
+
+class Spinner:
+    busy = False
+    delay = 0.4
+
+    @staticmethod
+    def spinning_cursor():
+        while 1:
+            for cursor in '|/-\\': yield cursor
+
+    def __init__(self, delay=None):
+        self.spinner_generator = self.spinning_cursor()
+        if delay and float(delay): self.delay = delay
+
+    def spinner_task(self):
+        while self.busy:
+            sys.stdout.write(next(self.spinner_generator))
+            sys.stdout.flush()
+            time.sleep(self.delay)
+            sys.stdout.write('\b')
+            sys.stdout.flush()
+
+    def start(self):
+        self.busy = True
+        threading.Thread(target=self.spinner_task).start()
+
+    def stop(self):
+        self.busy = False
+        time.sleep(self.delay)
+
+
+headers = {
+    "user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36",
+    'accept': '*/*'
+}
+url_cities = "https://apigate.tui.ru/api/office/cities"
+date = []
+phones = []
+
+
+def tui_pars():
+    request_cities = requests.get(url_cities, headers=headers)
+    if request_cities.status_code == 200:
+        print("Идет процесс парсинга")
+        spinner = Spinner()
+        spinner.start()
+        for id_city in request_cities.json()['cities']:
+            url_office = f"https://apigate.tui.ru/api/office/list?cityId={id_city['cityId']}&subwayId=&hoursFrom=&hoursTo=&serviceIds=all&toBeOpenOnHolidays=false"
+            request_offices = requests.get(url_office, headers=headers)
+            for office in request_offices.json()['offices']:
+                phones.clear()
+                for i in office['phones']:
+                    phones.append(i['phone'])
+                    phones.append(i['url'])
+                if office['hoursOfOperation']['saturday']['isDayOff'] == False:
+                    saturday = f"{office['hoursOfOperation']['saturday']['startStr']} до {office['hoursOfOperation']['saturday']['endStr']}"
+                else:
+                    saturday = "выходной"
+                if office['hoursOfOperation']['sunday']['isDayOff'] == False:
+                    sunday = f"{office['hoursOfOperation']['sunday']['startStr']} до {office['hoursOfOperation']['sunday']['endStr']}"
+                else:
+                    sunday = "выходной"
+                date.append({
+                    'address': office['address'],
+                    'latlon': [float(office['latitude']), float(office['longitude'])],
+                    'name': office['name'],
+                    'phones': phones,
+                    'working_hours': [
+                        f"будние дни {office['hoursOfOperation']['workdays']['startStr']} до {office['hoursOfOperation']['workdays']['endStr']}",
+                        f"суббота {saturday}",
+                        f"воскресенье {sunday}"
+                    ]
+                })
+        spinner.stop()
+        print("Процесс окончен")
+    with open('data_site_2.json', 'w') as file:
+        json.dump(date, file, indent=4, ensure_ascii=False)
+
+
+tui_pars()
